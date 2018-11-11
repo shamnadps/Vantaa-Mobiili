23df17d3fd606e8494378ecfd99d3a40b9b3631a
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-11-11 14:50:54 +0200

    removed splash screen dependency

--- App.js
+++ App.js
@@ -4,14 +4,9 @@ import AppNavigator from './navigation/AppNavigator';
 import { createStore } from 'redux';
 import reducer from './redux/reducer';
 import { Provider } from 'react-redux';
-import SplashScreen from 'react-native-splash-screen';
 
 const store = createStore(reducer);
 export default class App extends React.Component {
-
-  componentDidMount() {
-    SplashScreen.hide()
-  }
 
   render() {
     return (
